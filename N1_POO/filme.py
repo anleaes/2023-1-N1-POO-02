723c6a0ccf23789c9517efe0c7604470e6865ed1
CarlosKSL <81201191+CarlosKSL@users.noreply.github.com>
2023-04-17 16:20:21 -0300

    Add files via upload
    Inicio de projeto

---
+++ N1_POO/filme.py
@@ -0,0 +1,9 @@
+class Filme():
+    
+    def __init__(self, titulo, sinopse, genero, classificacao, elenco):
+        self.titulo = titulo
+        self.sinopse = sinopse
+        self.genero = genero
+        self.classificacao = classificacao
+        self.elenco = elenco
+        
---
+++ N1_POO/pessoa.py
@@ -0,0 +1,13 @@
+class Pessoa():
+    
+    def __init__(self, email, senha, cpf, nome, telefone, endereco, dataNascimento):
+        self.email = email
+        self.senha = senha
+        self.cpf = cpf
+        self.nome = nome
+        self.telefone = telefone
+        self.endereco = endereco
+        self.dataNascimento = dataNascimento
+        
+        
+        
