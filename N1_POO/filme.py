2b06da1ceacc966fe24d1ec4773a9dcf17afe133
CarlosKSL <81201191+CarlosKSL@users.noreply.github.com>
2023-04-17 19:56:40 -0300

    Delete N1_POO directory

--- N1_POO/filme.py
+++
@@ -1,9 +0,0 @@
-class Filme():
-    
-    def __init__(self, titulo, sinopse, genero, classificacao, elenco):
-        self.titulo = titulo
-        self.sinopse = sinopse
-        self.genero = genero
-        self.classificacao = classificacao
-        self.elenco = elenco
-        
